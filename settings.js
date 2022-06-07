153712ec50ffc80f7a35b752f949ad3669470cfd
WonXd <103139749+WonXd@users.noreply.github.com>
2022-06-08 03:07:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -67,7 +67,7 @@
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
-  global.thumb = fs.readFileSync('./media/hinata.jpg')
+  global.thumb = fs.readFileSync('./lib/rixzz.jpg')
     
   // Ini Gausah Diubah
   let file = require.resolve(__filename)
